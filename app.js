c19295470ad38c8a877b77d2579475c2613db358
giuliabriao <briaogiulia@gmail.com>
2021-03-26 18:41:02 -0300

    API feita

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var faker = require('faker');
+const faker = require('faker');
+const express = require('express');
+const app = express();
+const port = 8080;
 
 faker.locale = 'pt_BR';
 
@@ -6,7 +9,26 @@ faker.locale = 'pt_BR';
     let randomLastName = faker.name.lastName();
     let gender = faker.name.gender();
     let randomEmail = faker.internet.email(randomFirstName, randomLastName);
-    let past = faker.date.past(65, 2020);
 
-// console.log(`Nome: ${randomFirstName}, e-mail: ${randomEmail}`);
-console.log(past);
+    let dataFake = faker.date.past(65, 2020);
+
+    let dataSemAspas = JSON.stringify(dataFake).split('"')[1];
+    let apenasData = dataSemAspas.split("T")[0];
+    let [yyyy, mm, dd] = apenasData.split("-");
+    let bday = `${dd}/${mm}/${yyyy}`;
+    
+    app.get('/', (req, res) => {
+      res.send(randomUser = [{
+        
+        randomFirstName,
+        randomLastName,
+        gender,
+        bday,
+        randomEmail,
+
+      }])
+    });
+
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`)
+    });
